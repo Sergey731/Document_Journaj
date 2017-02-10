c941ac64ba334ee1ae09163214af9e36b13e5121
Sergey731 <psv.731@yandex.ru>
2017-02-10 12:07:12 +0500

    First step(3 needed)

---
+++ journal.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Просмотр документов</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
+</head>
+<body>
+	<?php
+
+	$marker = 'C:\Users\home\workspace\PhpProjects\Document_Journal\texts';
+	$storage = [];
+		
+	foreach (glob('C:\Users\home\workspace\PhpProjects\Document_Journal\texts\*.txt') as $file)			
+		 	array_push($storage, $file);		 	
+		
+		echo '<ul>';
+	    for ($i = 0; $i < count($storage); $i++) {
+	    	$file_name = substr($storage[$i], strlen($marker) + 1);
+	    	echo "<li>$file_name</li>";       
+	    }
+	    echo '</ul>';
+	
+	    $len = count($storage);
+	    echo "В папке " . $len . " документа(ов).";
+
+
+
+
+
+
+
+
+	?>	
+</body>
+</html>
